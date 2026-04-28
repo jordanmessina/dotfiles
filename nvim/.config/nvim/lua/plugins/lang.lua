032b602d132ba4f033644d940d23953c34baabd8
Jordan Messina <messinajordan@gmail.com>
2026-04-27 20:56:44 -0400

    nvim plugin order

--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -1,4 +1,3 @@
-return {
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.json" },
-}
+-- LazyVim extras must be imported before the custom `plugins` layer.
+-- They live in `lua/config/lazy.lua` so LazyVim sees them in the correct order.
+return {}
